52a920c19291fdc3850e25d98d7e7de5ece432f6
Shivang Rathore <rshivang12345@gmail.com>
2025-08-05 11:35:49 +0530

    chore: random stuff

--- apps/web/features/post-compose/hooks/use-attachments.tsx
+++ apps/web/features/post-compose/hooks/use-attachments.tsx
@@ -3,7 +3,7 @@
 import { Attachment } from "@repo/types";
 import { useQuery } from "@tanstack/react-query";
 import { getAttachments } from "../api/upload";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
 export function useAttachments(postId: number | undefined) {
   const {
@@ -12,21 +12,15 @@ export function useAttachments(postId: number | undefined) {
     refetch,
   } = useQuery<Attachment[]>({
     queryKey: ["attachments", postId],
-    queryFn: async () => getAttachments(postId as number),
-    enabled: !!postId,
+    queryFn: async () => {
+      if (!postId) return [];
+      return await getAttachments(postId);
+    },
     refetchOnWindowFocus: false,
     refetchOnReconnect: false,
   });
 
   const [attachments, setAttachments] = useState<Attachment[]>(data);
-  const firstLoad = useRef(true);
-
-  useEffect(() => {
-    if (firstLoad.current) {
-      firstLoad.current = false;
-      setAttachments(data);
-    }
-  }, [data]);
 
   return {
     draftAttachments: attachments,
